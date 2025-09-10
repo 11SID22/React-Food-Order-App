572a1d2fa3a5f43610c44f446a182774bdabe138
11SID22 <sid111097@gmail.com>
2025-09-10 11:58:25 +0530

    feat: implement order submission via fetch in Checkout form.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -22,6 +22,22 @@ const Checkout = () => {
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
@@ -32,7 +48,7 @@ const Checkout = () => {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)} </p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
