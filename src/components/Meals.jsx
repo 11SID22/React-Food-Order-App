46b7e6a9665938385458ac82b4be43bc666e4a9c
11SID22 <sid111097@gmail.com>
2025-09-11 09:35:45 +0530

    feat: add Error component and integrate loading & error handling in Meals.

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 
 const requestConfig = {};
 
@@ -9,6 +10,14 @@ const Meals = () => {
     isLoading,
     error,
   } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  if (isLoading) {
+    return <p className="center">Fetching meals...</p>;
+  }
+
+  if (error) {
+    return <Error title="Failed to fetch meals." message={error} />;
+  }
 
   return (
     <ul id="meals">
